fc42cf2c278726421d47118cc966d24780fed6eb
Maria Campbell <interglobalmedia@gmail.com>
2022-01-02 16:17:14 -0500

    Add lint formatting

--- scripts/js/carousel.js
+++ scripts/js/carousel.js
@@ -1,94 +1,94 @@
-window.addEventListener("DOMContentLoaded", () => {
-  let slideIndex = 0;
+window.addEventListener('DOMContentLoaded', () => {
+	let slideIndex = 0
 
-  carousel();
+	carousel()
 
-  function carousel() {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 4500);
-  }
+	function carousel() {
+		let i
+		let slides = document.getElementsByClassName('slide')
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = 'none'
+		}
+		slideIndex++
+		if (slideIndex > slides.length) {
+			slideIndex = 1
+		}
+		slides[slideIndex - 1].style.display = 'block'
+		setTimeout(carousel, 4500)
+	}
 
-  // web audio api related
-  const audioContext = new AudioContext();
-  // consolidated version
-  /* const audioContext = new(window.AudioContext || window.webkitAudioContext)(); */
-  // get the audio element
-  const audioElement = document.querySelector("audio");
-  /* sets the loop attribute dynamically by setting the loop property on the audio element. */
-  audioElement.loop = true;
-  // pass it into the audio context
-  // noinspection JSCheckFunctionSignatures
-  const track = audioContext.createMediaElementSource(audioElement);
+	// web audio api related
+	const audioContext = new AudioContext()
+	// consolidated version
+	/* const audioContext = new(window.AudioContext || window.webkitAudioContext)(); */
+	// get the audio element
+	const audioElement = document.querySelector('audio')
+	/* sets the loop attribute dynamically by setting the loop property on the audio element. */
+	audioElement.loop = true
+	// pass it into the audio context
+	// noinspection JSCheckFunctionSignatures
+	const track = audioContext.createMediaElementSource(audioElement)
 
-  const gainNode = audioContext.createGain();
+	const gainNode = audioContext.createGain()
 
-  track.connect(gainNode).connect(audioContext.destination);
+	track.connect(gainNode).connect(audioContext.destination)
 
-  // select the play button
-  const playButton = document.querySelector(".play-pause");
+	// select the play button
+	const playButton = document.querySelector('.play-pause')
 
-  playButton.addEventListener(
-    "click",
-    function () {
-      if (audioContext.state === "suspended") {
-        // noinspection JSIgnoredPromiseFromCall
-        audioContext.resume();
-      }
-      // play or pause track
-      if (this.dataset.playing === "false") {
-        audioElement.play();
-        this.dataset.playing = "true";
-        playButton.innerHTML = `Pause`;
-      } else if (this.dataset.playing === "true") {
-        audioElement.pause();
-        this.dataset.playing = "false";
-        playButton.innerHTML = `Play`;
-      }
-    },
-    false
-  );
-  audioElement.addEventListener("ended", () => {
-    playButton.dataset.playing = "false";
-  });
-  const volumeControl = document.querySelector("#volume");
-  volumeControl.addEventListener(
-    "input",
-    function () {
-      gainNode.gain.value = this.value;
-    },
-    false
-  );
-});
+	playButton.addEventListener(
+		'click',
+		function () {
+			if (audioContext.state === 'suspended') {
+				// noinspection JSIgnoredPromiseFromCall
+				audioContext.resume()
+			}
+			// play or pause track
+			if (this.dataset.playing === 'false') {
+				audioElement.play()
+				this.dataset.playing = 'true'
+				playButton.innerHTML = 'Pause'
+			} else if (this.dataset.playing === 'true') {
+				audioElement.pause()
+				this.dataset.playing = 'false'
+				playButton.innerHTML = 'Play'
+			}
+		},
+		false
+	)
+	audioElement.addEventListener('ended', () => {
+		playButton.dataset.playing = 'false'
+	})
+	const volumeControl = document.querySelector('#volume')
+	volumeControl.addEventListener(
+		'input',
+		function () {
+			gainNode.gain.value = this.value
+		},
+		false
+	)
+})
 
 window.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    const stage1 = document.getElementById("image-wrapper");
-    const arr1 = stage1.getElementsByTagName("div");
-    const stage2 = document.getElementById("image-wrapper");
-    const arr2 = stage2.getElementsByTagName("img");
-    const fadeComplete1 = function () {
-      stage1.appendChild(arr1[0]);
-    };
-    const fadeComplete2 = function () {
-      stage2.appendChild(arr2[1]);
-    };
+	'DOMContentLoaded',
+	function () {
+		const stage1 = document.getElementById('image-wrapper')
+		const arr1 = stage1.getElementsByTagName('div')
+		const stage2 = document.getElementById('image-wrapper')
+		const arr2 = stage2.getElementsByTagName('img')
+		const fadeComplete1 = function () {
+			stage1.appendChild(arr1[0])
+		}
+		const fadeComplete2 = function () {
+			stage2.appendChild(arr2[1])
+		}
 
-    for (let i = 0; i < arr1.length; i++) {
-      arr2[i].addEventListener("animationend", fadeComplete1, false);
-      for (let j = 0; j < arr2.length; j++) {
-        arr2[j].addEventListener("animationend", fadeComplete2, false);
-      }
-    }
-  },
-  false
-);
+		for (let i = 0; i < arr1.length; i++) {
+			arr2[i].addEventListener('animationend', fadeComplete1, false)
+			for (let j = 0; j < arr2.length; j++) {
+				arr2[j].addEventListener('animationend', fadeComplete2, false)
+			}
+		}
+	},
+	false
+)
